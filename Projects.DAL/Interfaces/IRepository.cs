cf059a5119f5fd3c1b0b01c719816136def04c7e
Олег Терентьев <oleg.terentev123456789@outlook.com>
2021-06-29 17:50:02 +0300

    Create general repository class

---
+++ Projects.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using Projects.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projects.DAL.Interfaces
+{
+    interface IRepository<T> where T : TEntity
+    {
+        IEnumerable<T> Get(Expression<Func<T, bool>> filter = null);
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void Delete(int id);
+    }
+}
