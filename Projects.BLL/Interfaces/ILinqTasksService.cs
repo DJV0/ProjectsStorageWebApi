f95c1eb7150d236a943f474b6fe14e57927d9c11
Олег Терентьев <oleg.terentev123456789@outlook.com>
2021-07-01 01:50:35 +0300

    Create linq tasks service.

---
+++ Projects.BLL/Interfaces/ILinqTasksService.cs
@@ -0,0 +1,19 @@
+﻿using Projects.BLL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Projects.BLL.Interfaces
+{
+    public interface ILinqTasksService
+    {
+        Dictionary<Project, int> GetProjectTasksCountByAuthorId(int authorId);
+        List<Task> GetPerformerTasks(int performerId);
+        List<(int Id, string Name)> GetFinishedPerformerTasks2021(int performerId);
+        List<Task4DTO> GetTeamsWhichMembersAgeOver10Years();
+        List<IGrouping<User, Task>> GetSortedUsers();
+        Task6DTO GetTask6(int userId);
+        List<Task7DTO> GetTask7();
+    }
+}
