c2e15d2811a17c453bbf4246f401bdeb4cce479e
Олег Терентьев <oleg.terentev123456789@outlook.com>
2021-06-30 18:49:12 +0300

    Create project controller and service

--- Projects.BLL/Services/ProjectService.cs
+++ Projects.BLL/Services/ProjectService.cs
@@ -9,8 +9,54 @@ using System.Threading.Tasks;
 
 namespace Projects.BLL.Services
 {
-    public class ProjectService : GenericService<Project>, IProjectService
+    public class ProjectService :  IProjectService
     {
-        public ProjectService(IProjectRepository context) : base(context) { }
+        private readonly IUnitOfWork _context;
+        public ProjectService(IUnitOfWork context)
+        {
+            _context = context;
+        }
+
+        public void Add(Project project)
+        {
+            if (!ExistTeam(project.TeamId) || !ExistAuthor(project.AuthorId) || project.Deadline < DateTime.Now) return;
+            project.CreatedAt = DateTime.Now;
+            _context.ProjectRepository.Create(project);
+        }
+
+        public void Delete(Project project)
+        {
+            _context.ProjectRepository.Delete(project);
+        }
+
+        public void Delete(int id)
+        {
+            _context.ProjectRepository.Delete(id);
+        }
+
+        public Project Get(int id)
+        {
+            return _context.ProjectRepository.Get(p => p.Id == id).FirstOrDefault();
+        }
+
+        public IEnumerable<Project> GetAll()
+        {
+            return _context.ProjectRepository.Get();
+        }
+
+        public void Update(Project project)
+        {
+            if (!ExistTeam(project.TeamId) || !ExistAuthor(project.AuthorId) || project.Deadline < DateTime.Now) return;
+            _context.ProjectRepository.Update(project);
+        }
+
+        private bool ExistTeam(int id)
+        {
+            return _context.TeamRepository.Get(t => t.Id == id).FirstOrDefault() != null ? true : false;
+        }
+        private bool ExistAuthor(int id)
+        {
+            return _context.UserRepository.Get(u => u.Id == id).FirstOrDefault() != null ? true : false;
+        }
     }
 }
---
+++ Projects.WebAPI/Controllers/ProjectsController.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Projects.BLL.Interfaces;
+using Projects.DAL.Models;
+using Projects.WebAPI.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Projects.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class ProjectsController : ControllerBase
+    {
+        private readonly IProjectService _projectService;
+        private readonly IMapper _mapper;
+
+        public ProjectsController(IProjectService projectService, IMapper mapper)
+        {
+            _projectService = projectService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Project>> Get()
+        {
+            return Ok(_projectService.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Project> Get(int id)
+        {
+            return Ok(_projectService.Get(id));
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] ProjectDTO projectDTO)
+        {
+            Project project = _mapper.Map<Project>(projectDTO);
+            _projectService.Add(project);
+            return CreatedAtAction(nameof(Get), new { id = project.Id }, project);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] ProjectDTO projectDTO)
+        {
+            if (id != projectDTO.Id) return BadRequest();
+            Project project = _mapper.Map<Project>(projectDTO);
+            _projectService.Update(project);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            _projectService.Delete(id);
+            return NoContent();
+        }
+    }
+}
