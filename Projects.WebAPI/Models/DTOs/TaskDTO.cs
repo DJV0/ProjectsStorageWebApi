6edf5c9fb70d4390c46b6e597280ae93b8969033
Олег Терентьев <oleg.terentev123456789@outlook.com>
2021-06-30 19:18:43 +0300

    Create task controller and service.

---
+++ Projects.WebAPI/Models/DTOs/TaskDTO.cs
@@ -0,0 +1,19 @@
+﻿using Projects.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projects.WebAPI.Models.DTOs
+{
+    public class TaskDTO
+    {
+        public int Id { get; set; }
+        public int ProjectId { get; set; }
+        public int PerformerId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public TaskState State { get; set; }
+        public DateTime? FinishedAt { get; set; }
+    }
+}
