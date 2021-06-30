11d6ec3088db55f173f870c214fd4489ac6edeed
Олег Терентьев <oleg.terentev123456789@outlook.com>
2021-06-30 17:49:47 +0300

    Create user controller and service. Delete generic service.

--- Projects.BLL/Services/UserService.cs
+++ Projects.BLL/Services/UserService.cs
@@ -9,8 +9,50 @@ using System.Threading.Tasks;
 
 namespace Projects.BLL.Services
 {
-    public class UserService : GenericService<User>, IUserService
+    public class UserService : IUserService
     {
-        public UserService(IUserRepository context) : base(context) { }
+        private readonly IUnitOfWork _context;
+        public UserService(IUnitOfWork context)
+        {
+            _context = context;
+        }
+
+        public void Add(User user)
+        {
+            if (user.TeamId != null && !ExistTeam(user.TeamId)) return;
+            user.RegisteredAt = DateTime.Now;
+            _context.UserRepository.Create(user);
+        }
+
+        public void Delete(User user)
+        {
+            _context.UserRepository.Delete(user);
+        }
+
+        public void Delete(int id)
+        {
+            _context.UserRepository.Delete(id);
+        }
+
+        public User Get(int id)
+        {
+            return _context.UserRepository.Get(u => u.Id == id).FirstOrDefault();
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return _context.UserRepository.Get();
+        }
+
+        public void Update(User user)
+        {
+            if (user.TeamId != null && !ExistTeam(user.TeamId)) return;
+            _context.UserRepository.Update(user);
+        }
+
+        private bool ExistTeam(int? id)
+        {
+            return _context.TeamRepository.Get(t => t.Id == id).FirstOrDefault() != null ? true : false;
+        }
     }
 }
