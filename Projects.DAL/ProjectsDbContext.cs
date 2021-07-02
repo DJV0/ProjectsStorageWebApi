bd874255847877cdbc7de47f5cc130cce24b9216
Олег Терентьев <oleg.terentev123456789@outlook.com>
2021-07-02 20:01:23 +0300

    Install EF Core. Create DBContext

---
+++ Projects.DAL/ProjectsDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Projects.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Projects.DAL
+{
+    public class ProjectsDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<Project> Projects { get; set; }
+
+        public ProjectsDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Tasks)
+                .WithOne(t => t.Performer)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
