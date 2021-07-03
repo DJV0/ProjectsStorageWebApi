00ad7f062085f7f6b791efefe350758f03bc6ce8
Олег Терентьев <oleg.terentev123456789@outlook.com>
2021-07-03 22:01:27 +0300

    Change UnitOfWork to DbContext. Edit crud service methods to work with DbContext.

--- Projects.BLL/Services/TeamService.cs
+++ Projects.BLL/Services/TeamService.cs
@@ -4,15 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Projects.BLL.Interfaces;
-using Projects.DAL.Interfaces;
-using Projects.DAL.Models;
+using Projects.DAL.Entities;
+using Projects.DAL;
+using Microsoft.EntityFrameworkCore;
 
 namespace Projects.BLL.Services
 {
     public class TeamService : ITeamService
     {
-        private readonly IUnitOfWork _context;
-        public TeamService(IUnitOfWork context)
+        private readonly ProjectsDbContext _context;
+        public TeamService(ProjectsDbContext context)
         {
             _context = context;
         }
@@ -20,32 +21,39 @@ namespace Projects.BLL.Services
         public void Add(Team team)
         {
             team.CreatedAt = DateTime.Now;
-            _context.TeamRepository.Create(team);
+            _context.Teams.Add(team);
+            _context.SaveChanges();
         }
 
         public void Delete(Team team)
         {
-            _context.TeamRepository.Delete(team);
+            _context.Teams.Remove(team);
+            _context.SaveChanges();
         }
 
         public void Delete(int id)
         {
-            _context.TeamRepository.Delete(id);
+            var team = _context.Teams.Find(id);
+            if (team != null) Delete(team);
         }
 
         public Team Get(int id)
         {
-            return _context.TeamRepository.Get(t => t.Id == id).FirstOrDefault();
+            return _context.Teams
+                .FirstOrDefault(team => team.Id == id);
         }
 
         public IEnumerable<Team> GetAll()
         {
-            return _context.TeamRepository.Get();
+            return _context.Teams
+                .ToList();
         }
 
         public void Update(Team team)
         {
-            _context.TeamRepository.Update(team);
+            _context.Teams.Attach(team);
+            _context.Entry(team).Property(t => t.Name).IsModified = true;
+            _context.SaveChanges();
         }
     }
 }
